8502df5f627b95e6ed211af489c7ce96cf490b44
wh002 <wanghao002@sensorsdata.cn>
2023-05-21 21:02:32 +0800

    fix

--- src/runtime/test/ranger_resource_policy_test.cpp
+++ src/runtime/test/ranger_resource_policy_test.cpp
@@ -162,7 +162,7 @@ TEST(ranger_resource_policy_test, acl_policies_allowed)
         auto actual_result = policy.policies_check(test.ac_type, test.user_name, test.check_type);
         EXPECT_EQ(test.expected_result, actual_result)
             << fmt::format("ac_type: {}, user_name: {}, check_type: {}",
-                           enum_to_string(test.ac_type),
+                           test.ac_type,
                            test.user_name,
                            enum_to_string(test.check_type));
     }
